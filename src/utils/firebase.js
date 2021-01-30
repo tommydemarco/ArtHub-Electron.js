d691fff9303ff934e78fd6bfa1b28beb5c8e1dc4
tommydemarco <tommaso.demarco95@gmail.com>
2021-01-30 23:33:33 +0100

    Added firebase to the project

---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhxFLOpImJNtt_63LPeIKD4yAc304xHJo",
+  authDomain: "arty-hub.firebaseapp.com",
+  projectId: "arty-hub",
+  storageBucket: "arty-hub.appspot.com",
+  messagingSenderId: "993356909255",
+  appId: "1:993356909255:web:2cdbc0d261307768b2cce2",
+};
+
+module.exports = firebase.initializeApp(firebaseConfig);
